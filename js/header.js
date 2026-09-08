0499012995ee8f98d234108aa162f905de248229
Anastasiia Rebenok <ARebenok@ctc-lohr.net>
2026-09-09 01:28:21 +0200

    refine desktop layout and header interactions

--- js/header.js
+++ js/header.js
@@ -2,10 +2,22 @@ function initHeader() {
   const overlay = document.querySelector(".header__overlay");
   const menuButton = document.querySelector(".header__menu-button");
   const menuText = document.querySelector(".header__menu-text");
+  const contactButton = document.querySelector(".header__contact");
+  const languageSwitcher = document.querySelector(".header__language-switcher");
+  const languageButton = document.querySelector(".header__language-button");
+
+  const languageOptions = document.querySelectorAll(
+    ".header__language-menu button",
+  );
 
   const homeLinks = document.querySelectorAll(
     ".header__logo, .header__overlay-logo, #home-link",
   );
+
+  if (document.body.classList.contains("body-contact-page")) {
+    contactButton.textContent = "PROJECTS";
+    contactButton.href = "./index.html";
+  }
 
   homeLinks.forEach((link) => {
     link.addEventListener("click", () => {
@@ -26,11 +38,23 @@ function initHeader() {
   function closeMenu() {
     document.body.classList.remove("menu-open");
     menuText.textContent = "MENU";
+    languageSwitcher.classList.remove("is-open");
 
     setTimeout(() => {
       overlay.classList.remove("is-open");
     }, 1100);
   }
+
+  languageButton.addEventListener("click", () => {
+    languageSwitcher.classList.toggle("is-open");
+  });
+
+  languageOptions.forEach((option) => {
+    option.addEventListener("click", () => {
+      languageButton.textContent = option.textContent;
+      languageSwitcher.classList.remove("is-open");
+    });
+  });
 
   menuButton.addEventListener("click", () => {
     if (document.body.classList.contains("menu-open")) {
@@ -38,6 +62,7 @@ function initHeader() {
     } else {
       overlay.classList.add("is-open");
       document.body.classList.add("menu-open");
+      document.body.classList.add("scroll-locked");
       menuText.textContent = "CLOSE";
     }
   });
